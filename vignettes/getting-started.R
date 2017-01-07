56ecda224daf0747c52662fbb05879d238df9e05
spocks <arvindmer@gmail.com>
2017-01-06 20:58:56 -0500

    changed

--- vignettes/getting-started.R
+++ vignettes/getting-started.R
@@ -1,28 +1,16 @@
 ## ----setup, include=FALSE------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE)
 
-## ---- echo=TRUE----------------------------------------------------------
+## ----load, echo=TRUE-----------------------------------------------------
 library(Xeva)
-data(pdxe)
+cdf = readRDS("~/CXP/Xeva/DATA-raw/celineData.Rds")
+head(ModelInfo(cdf))
 
 ## ---- echo=TRUE----------------------------------------------------------
-#modelIds = getModelIds(pdxe, drug="paclitaxel", drug.match.exact=TRUE, tumor.type="BRCA")
-#print(modelIds)
-df = getExperiment(pdxe, experiment.id="X.1004.pael.paclitaxel")
-head(df)
+print(cdf@expDesign[[1]]) 
 
 ## ---- echo=TRUE----------------------------------------------------------
-df = getExperiment(pdxe, experiment.id="X.1004.pael.paclitaxel")
-head(df)
-
-## ---- echo=TRUE----------------------------------------------------------
-library(Xeva)
-data("celineData")
-head(ModelInfo(lpdx))
-
-## ----cars----------------------------------------------------------------
-summary(cars)
-
-## ----pressureX, , include=TRUE, echo=FALSE-------------------------------
-plot(pressure)
+batch = cdf@expDesign[[1]]
+ang = calculateAngle(cdf, batch, plot=TRUE)
+print(ang)
 
