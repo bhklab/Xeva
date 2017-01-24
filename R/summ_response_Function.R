9f144274646bd8f1f248a74b8e9bd9e818e9523d
spocks <arvindmer@gmail.com>
2017-01-24 17:10:39 -0500

    fixed bug in summarizeResponse

--- R/summ_response_Function.R
+++ R/summ_response_Function.R
@@ -1,20 +1,20 @@
 
 
 ###----sensitivity/response ----------
-
-make_response_slot <- function(object)
-{
-  allDrg = unique(unlist(sapply(object@experiment, "[[", c("drug", "join.name"))))
-  allMod = unique(unlist(sapply(object@experiment, "[[", "model.id")))
-  modRes = matrix(NA, nrow = length(allDrg), ncol = length(allMod))
-
-  for(I in object@experiment)
-  {
-
-
-  }
-
-}
+#
+# make_response_slot <- function(object)
+# {
+#   allDrg = unique(unlist(sapply(object@experiment, "[[", c("drug", "join.name"))))
+#   allMod = unique(unlist(sapply(object@experiment, "[[", "model.id")))
+#   modRes = matrix(NA, nrow = length(allDrg), ncol = length(allMod))
+#
+#   for(I in object@experiment)
+#   {
+#
+#
+#   }
+#
+# }
 
 .getDrugsForABatch <- function(object, batch)
 {
@@ -36,18 +36,57 @@ make_response_slot <- function(object)
                                 drug.join.name=drug,
                                 angle= batch$angle))
   }
+
+  rdf <- .factor2char(rdf)
   return(rdf)
 }
 
-.summarizePerBatchResponse <- function(object, response.measure = "angle")
+.summarizePerBatchResponse <- function(object, response.measure = "angle", group.by=NULL, summary.stat ="mean")
 {
+
   if(response.measure == "angle")
   {
     df = .getAngleSummary(object)
   }
 
-  mat = .castDataFram(df, row.var="drug.join.name", col.var = "batch.name",
-                      value=response.measure, collapse = ";")
+  if(!is.null(group.by) & group.by != "batch.name")
+  {
+    mapId = mapModelSlotIds(object, id=df$batch.name, id.name="batch.name",
+                            map.to=group.by, unique = TRUE)
+
+    for(I in unique(mapId$batch.name))
+    {
+      di = unique(mapId[mapId$batch.name==I, group.by])
+      if(length(di)>1)
+      {
+        msg1 = sprintf("batch.name mapped to multipal %s therefore all such %s will contain same information", group.by, group.by)
+        msg2 = sprintf("\nbatch.name = %s %s = %s\n", I, group.by, paste(di, collapse =","))
+        warning(msg1, msg2)
+      }
+    }
+
+    df = merge(df, mapId, by.x = "batch.name", by.y="batch.name")
+
+    for(I in unique(df[, group.by]))
+    {
+      di = df[df[, group.by]==I, ]
+      if(nrow(di)>1)
+      {
+        drg <- di$drug.join.name
+        if(length(drg) > length(unique(drg)))
+        {
+          msg1 <- sprintf("'%s' mapped to multipal batch.name, values will be collapsed using '%s'\n", group.by, summary.stat)
+          df2Pr<- unique(di[, c("batch.name", group.by)]); rownames(df2Pr)<- NULL
+          msg2 <- paste(capture.output(print(df2Pr)), collapse = "\n")
+          warning(msg1, msg2)
+        }
+      }
+    }
+  }
+
+  mat = .castDataFram(df, row.var="drug.join.name", col.var = group.by,
+                      value=response.measure, collapse = summary.stat)
+
   return(mat)
 }
 
@@ -94,11 +133,6 @@ getValueFromModel <- function(object, model.id, values)
                 values=c("model.id","drug.join.name", "slop")
                 getValueFromModel(object, model.id, values)} )
     df = .convertListToDataFram(df)
-    #if(any(is.na(df$slop)))
-    #{
-    #  df = df[!is.na(df$slop), ]
-    #  warning("removing slope with NA")
-    #}
     valueColName = "slop"
   }
 
@@ -115,6 +149,10 @@ getValueFromModel <- function(object, model.id, values)
 
 
 
+#.checkGroupByValue <- function(object, group.by, response.measure)
+#{
+#  if(group.by=="batch.name" & )
+#}
 
 #####================= summarizeResponse ==================
 #' Summarize Response of PDXs
@@ -123,8 +161,8 @@ getValueFromModel <- function(object, model.id, values)
 #'
 #' @param object The \code{XevaSet}
 #' @param response.measure \code{character} . Which response measure to use? Use the responseMeasures function to find out what measures are available for each Xeva set.
-#' @param group.by default \code{patient.id}. How the models should be grouped togather.
-#' @param summary.stat which summary method to use if more then multipal model.id response is found
+#' @param group.by default \code{patient.id}. How the models should be grouped togather. See details
+#' @param summary.stat which summary method to use if multipal ids were found
 #' @return a \code{matrix} with rows as drug names, coulmn as \code{group.by} and each cell contains \code{response.measure} for the pair.
 #'
 #' @details
@@ -134,7 +172,7 @@ getValueFromModel <- function(object, model.id, values)
 #' \item{per batch response : One response value for each Batch, e.g. angle between treatment and control groups}
 #' }
 #' In case of \code{per model response} output columns will be \code{model.id} (or group.by).
-#' For \code{per batch response} output columns will be \code{batch.name}.
+#' For \code{per batch response} \code{group.by} value can be \code{"batch.name"} .
 #'
 #' @examples
 #' data(pdxe)
@@ -145,6 +183,15 @@ summarizeResponse <- function(object, response.measure = "mRECIST_recomputed",
 {
 
   summary.stat = c(summary.stat)[1]
+
+  if(group.by=="batch.name")
+  {
+    if(is.element(response.measure, c("angle"))==FALSE)
+    {
+      msg1 = sprintf("group.by== 'batch.name' is only allowed in per batch response" )
+      stop(msg1)
+    }
+  }
 
   if(response.measure =="mRECIST_recomputed")
   {
@@ -160,7 +207,9 @@ summarizeResponse <- function(object, response.measure = "mRECIST_recomputed",
 
   if(response.measure =="angle")
   {
-    mat <- .summarizePerBatchResponse(object, response.measure = "angle")
+    mat <- .summarizePerBatchResponse(object, response.measure = "angle",
+                               group.by=group.by, summary.stat=summary.stat)
+
   }
 
   return(mat)
