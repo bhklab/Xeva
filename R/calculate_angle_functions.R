028474fcdfa12af4bf0f28f01df1e9a188cd2633
spocks <arvindmer@gmail.com>
2017-03-20 11:23:58 -0400

    changed angle calculation functions

--- R/calculate_angle_functions.R
+++ R/calculate_angle_functions.R
@@ -1,13 +1,19 @@
 
 
-.computSlopFun <- function(x,y, log.y=TRUE)
-{
-  #fit = lm(y~x)
-  if(log.y==TRUE)
-  {y = log(y)}
+.computAngle <- function(a,b){
+  if(length(a)>4 | length(b)>4)
+  {
+    msg <- sprintf("Only give start and end points of the lines. For example
+                   a = c(start.point.x, start.point.y, end.point.x, end.point.y)")
+    stop(msg)
+  }
+  theta <- acos( sum(a*b) / ( sqrt(sum(a * a)) * sqrt(sum(b * b)) ) )
+  as.numeric(theta)*180/base::pi
+}
 
-  #print(log(y))
-  ##-------------------------------------------------------------
+
+.computSlopFun <- function(x,y)
+{
   data = data.frame(x=x, y=y)
   ##---- remove all non finite (Inf, NA, NaN) data --------------
   data = data[is.finite(data$x), ]
@@ -15,111 +21,14 @@
 
   p= data$x[1]; q = data$y[1]
   fit <- (lm(I(y-q)~I(x-p) +0, data))
-  ##------------------------------------------------
-  #ang = atan(coef(fit)[["x"]]) *180 / pi
-  ang = atan(coef(fit)[["I(x - p)"]]) *180/3.141593
+  ang <- atan(coef(fit)[["I(x - p)"]]) *180/base::pi
+
   return(list(fit=fit, angel=ang, data=data))
 }
 
-#
-# .plotAngelAndFit <- function(dfT, dfC, fitT, fitC)
-# {
-#   xrng = range(dfT$time, dfC$time)
-#   yrng = range(dfT$mean, dfC$mean)
-#   opar <- par()      # make a copy of current settings
-#   par(pty="s",  xpd=TRUE)
-#   plot(dfT$time, dfT$mean, col="red",  pch=19,
-#        xlab = "time", ylab = "tumor volume",
-#        xlim = xrng, ylim = yrng)
-#   points(dfC$time,  dfC$mean, col="blue", pch=19)
-#
-#   legend("bottomright", inset=c(-0.38,0),
-#          legend=c("Treatment", "Control"), fill=c("red", "blue"), cex=0.8)
-#
-#   par(xpd=FALSE);
-#
-#   #nd = predict(fit, newdata = list(x=0))+q
-#   #.testPlot(x,y, nmod)
-#   #abline(nd, coef(nmod), col='blue')
-#   lxT = predict(fitT$fit, newdata = list(x=0))+dfT$mean[1]
-#   abline(lxT, coef(fitT$fit), col="red")
-#
-#   lxC = predict(fitC$fit, newdata = list(x=0))+dfC$mean[1]
-#   abline(lxC, coef(fitC$fit), col="blue")
-#
-#   #abline(fitT$fit, col="red"); abline(fitC$fit, col="blue")
-#
-#
-#   par(pty=opar$pty, xpd=opar$xpd) ## reset par to old setting
-# }
-#
-#
-# ####-----------------------------------------------------------
-# plot_Batch_angel <- function(dt.fit, dc.fit, fitC, fitT, dfC, dfT, title="plot")
-# {
-#   xrng = range(sapply(dt.fit, function(i) range(i$data$x)),
-#                sapply(dc.fit, function(i) range(i$data$x)))
-#
-#   yrng = range(sapply(dt.fit, function(i) range(i$data$y)),
-#                sapply(dc.fit, function(i) range(i$data$y)))
-#
-#   opar <- par()      # make a copy of current settings
-#   par(pty="s",  xpd=TRUE)
-#   plot(NA, col="red",  pch=19, main=title,
-#        xlab = "time", ylab = "tumor volume",
-#        xlim = xrng, ylim = yrng)
-#
-#   .plt1Mod <- function(d.fit, col){
-#   for(dfX in d.fit)
-#   {
-#     #points(dfX$data$x,  dfX$data$y, col="blue", pch=19)
-#     #lx = predict(dfX$fit, newdata = list(x=0))+dfX$data$y[1]
-#     #abline(lx, coef(dfX$fit), col="blue")
-#     lines(dfX$data$x,  dfX$data$y, col=col,type="b", lty=3, pch=19)
-#   } }
-#
-#   .plt1Mod(dc.fit, col = "#6baed6")
-#   .plt1Mod(dt.fit, col = "#fc8d59")
-#
-#   ##----- add line ------
-#   if(1==2){
-#   dfX =dc.fit[[5]]
-#   points(dfX$data$x,  dfX$data$y, col="blue", pch=19)
-#   lx = predict(dfX$fit, newdata = list(x=0))+dfX$data$y[1]
-#   yl = coef(dfX$fit)
-#   #avgAng = mean(sapply(dc.fit, "[[", "angel"))
-#   avgAng = mean(sapply(dc.fit, function(x)coef(x$fit)))
-#   yv = c(avgAng*xrng[1], avgAng*xrng[2])
-#   lines(xrng, yv, col="red")
-#   #lx1 = predict(dfX$fit, newdata = list(x=xrng[1]))+dfX$data$y[1]
-#   #lx2 = predict(dfX$fit, newdata = list(x=xrng[2]))+dfX$data$y[1]
-#   #abline(lx, coef(dfX$fit), col="blue")
-#   #lines(c(0,110), c(4.006887*0+lx, 4.006887*110+lx), col="red")
-#   }
-#
-#   legend("bottomright", inset=c(-0.38,0),
-#          legend=c("Treatment", "Control"),
-#          fill=c("#a50f15", "#081d58"), cex=0.8)
-#
-#   par(xpd=FALSE)
-#
-#   tPoint0 = mean(sapply(dt.fit, function(i) i$data$y[1]))
-#
-#   lxT = predict(fitT$fit, newdata = list(x=0))+ tPoint0 # dfT$y[1]
-#   abline(lxT, coef(fitT$fit), col="#a50f15")
-#
-#   cPoint0 = mean(sapply(dc.fit, function(i) i$data$y[1]))
-#   lxC = predict(fitC$fit, newdata = list(x=0))+ cPoint0 # dfC$y[1]
-#   abline(lxC, coef(fitC$fit), col="#081d58")
-#
-#   par(pty=opar$pty, xpd=opar$xpd)
-#
-# }
-#
-# ##--------------------------------------------------------------------
-
 #' @import ggplot2
-plot_Batch_angel_ggplot <- function(dt.fit, dc.fit, fitC, fitT, title="plot")
+plot_Batch_angel_ggplot <- function(dt.fit, dc.fit, liC, liT,
+                                    title="plot", log.y=FALSE)
 {
   ##-----make one DF ----------------------------------------
   dft = do.call(rbind, lapply(names(dt.fit), function(n)
@@ -131,28 +40,53 @@ plot_Batch_angel_ggplot <- function(dt.fit, dc.fit, fitC, fitT, title="plot")
   dfc$type = "control"
 
   DF = rbind(dft, dfc)
-  tcCol <- c("control" = "#6baed6", "treatment" = "#fc8d59")
 
+  ##-----normalize data to 0 and 1 --------------------------------
+  if(1==2){
+  dfXmin <- min(DF$x); dfXmax <- max(DF$x)
+  dfYmin <- min(DF$y); dfYmax <- max(DF$y)
+  normalize01 <- function(v, vmin, vmax) { (v-vmin)/(vmax-vmin) }
+
+  DF$x <- normalize01(DF$x, dfXmin, dfXmax)
+  DF$y <- normalize01(DF$y, dfYmin, dfYmax)
+
+  liC$x1 <- normalize01(liC$x1, dfXmin, dfXmax)
+  liC$x2 <- normalize01(liC$x2, dfXmin, dfXmax)
+  liC$y1 <- normalize01(liC$y1, dfYmin, dfYmax)
+  liC$y2 <- normalize01(liC$y2, dfYmin, dfYmax)
+
+  liT$x1 <- normalize01(liT$x1, dfXmin, dfXmax)
+  liT$x2 <- normalize01(liT$x2, dfXmin, dfXmax)
+  liT$y1 <- normalize01(liT$y1, dfYmin, dfYmax)
+  liT$y2 <- normalize01(liT$y2, dfYmin, dfYmax)
+  }
+  ##----------------------------------------------------------------
+  tcCol <- c("control" = "#6baed6", "treatment" = "#fc8d59")
   plt <- ggplot(DF, aes_string(x="x", y="y", color= "type", group="model.id"))
   plt <- plt + geom_line(linetype = 2)+ geom_point()
   plt <- plt + scale_color_manual(values=tcCol)
 
   ##-------add lm line -----------------------------------------------------------
-  .addLMfitLines <- function(plt, fit, p0, color="black")
+  plt <- plt+ geom_segment(aes(x = liC$x1, y = liC$y1, xend = liC$x2, yend = liC$y2), color="blue")
+  plt <- plt+ geom_segment(aes(x = liT$x1, y = liT$y1, xend = liT$x2, yend = liT$y2), color="red")
+
+  ##-------------------------------------------------------------------------------
+  plt <- plt + labs(title = title, x = "time", y = "volume", colour = "")
+
+  if(log.y==TRUE)
   {
-    lx = predict(fit, newdata = list(x=0))+ p0
-    plt + geom_abline(intercept = lx, slope = coef(fit)[1], color=color)
+    plt <- plt+scale_y_continuous(trans='log')
+    plt <- plt + labs(y = "log(volume)", colour = "")
+    #plt <- plt+ coord_fixed(ratio = (max(DF$x)-min(DF$x)) / ( log(max(DF$y))- log(min(DF$y))))
+
+  } else{
+    plt <- plt+ coord_fixed(ratio = (max(DF$x)-min(DF$x)) / (max(DF$y)-min(DF$y)))
   }
 
-  plt <- .addLMfitLines(plt, fit = fitC$fit, p0= fitC$data$y[1], color="blue")
-  plt <- .addLMfitLines(plt, fit = fitT$fit, p0= fitT$data$y[1], color="red")
-  ##-------------------------------------------------------------------------------
-  plt <- plt + theme(aspect.ratio=1)
+  #plt <- plt + theme(aspect.ratio=1)
   plt <- .ggplotEmptyTheme(plt)
-  plt <- plt + labs(title = title, x = "time", y = "log(volume)", colour = "")
   plt <- plt + theme(plot.title = element_text(hjust = 0.5))
   plt <- plt + theme(panel.border = element_rect(colour = "black", fill=NA, size=1))
-  #p + theme(legend.position='none')
   return(plt)
 }
 
@@ -160,8 +94,9 @@ plot_Batch_angel_ggplot <- function(dt.fit, dc.fit, fitC, fitT, title="plot")
 #' data(lpdx); object=lpdx
 #' expDegI  <- expDesign(lpdx, "PHLC111_P7")
 computAngelFor1ExpDesign <- function(object, expDegI, var="volume", treatment.only=TRUE,
-                                     plot=FALSE, log.y=TRUE)
+                                     plot=FALSE, log.y=FALSE)
 {
+
   ##------ for each model get fit ---------
   dt.fit= list()
   if(length(expDegI$treatment)>0)
@@ -170,7 +105,7 @@ computAngelFor1ExpDesign <- function(object, expDegI, var="volume", treatment.on
     {
       dt = getExperiment(object, model.id=ti, treatment.only = TRUE)
 
-      dt.fit[[ti]] = .computSlopFun(dt$time, dt[,var], log.y=log.y)
+      dt.fit[[ti]] = .computSlopFun(dt$time, dt[,var])
     }
   }
   ###-------------
@@ -180,12 +115,11 @@ computAngelFor1ExpDesign <- function(object, expDegI, var="volume", treatment.on
     for(ci in expDegI$control)
     {
       dc = getExperiment(object, model.id=ci, treatment.only = TRUE)
-      dc.fit[[ci]] = .computSlopFun(dc$time, dc[,var], log.y=log.y)
+      dc.fit[[ci]] = .computSlopFun(dc$time, dc[,var])
     }
   }
 
   DFx = getTimeVarData(object, expDegI, var=var, treatment.only=treatment.only)
-
   dfC = DFx[DFx$exp.type=="control",]
   dfT = DFx[DFx$exp.type=="treatment",]
 
@@ -200,24 +134,35 @@ computAngelFor1ExpDesign <- function(object, expDegI, var="volume", treatment.on
     warning("treatment have no data!")
     return(NA)
   }
-  fitC = .computSlopFun(dfC$time, dfC$mean, log.y=log.y)
-  fitT = .computSlopFun(dfT$time, dfT$mean, log.y=log.y)
-  angDiff = fitC$angel - fitT$angel
+  fitC = .computSlopFun(dfC$time, dfC$mean)
+  fitT = .computSlopFun(dfT$time, dfT$mean)
 
+  ##--------------------------------------------------------------------
+  .getLMfitLine <- function(fit, data)
+  {
+    x1 <- data$x[1]
+    x2 <- data$x[length(data$x)]
+    if(1==1){
+    intercept <- predict(fit, newdata = list(x=0))+ data$y[1]
+    y1 <- (as.numeric(coef(fit)) * x1) + intercept
+    y2 <- (as.numeric(coef(fit)) * x2) + intercept}
+    return(list(x1=x1, y1=y1, x2=x2,y2=y2))
+  }
+  liC <- .getLMfitLine(fit = fitC$fit, data=fitC$data)
+  liT <- .getLMfitLine(fit = fitT$fit, data=fitT$data)
+  ##--------------------------------------------------------------------
+
+  angDiff <- .computAngle(c(liC$x1, liC$y1, liC$x2, liC$y2), c(liT$x1, liT$y1, liT$x2, liT$y2))
+
+  plt <- NULL
   if(plot==TRUE)
   {
-    #plot_Batch_angel(dt.fit, dc.fit, fitC, fitT,dfC, dfT, title=expDegI$batch.name)
-    plt <- plot_Batch_angel_ggplot(dt.fit, dc.fit, fitC, fitT, title=expDegI$batch.name)
-    #print(plt)
-    return(list(angle= angDiff, plot=plt))
+    plt <- plot_Batch_angel_ggplot(dt.fit, dc.fit, liC, liT, title=expDegI$batch.name,
+                                   log.y=log.y)
   }
-
-  return(angDiff)
+  return(list(angle= angDiff, plot=plt))
+  #return(angDiff)
 }
-
-
-
-
 
 
 ## calculate angle between control and treatment groups
@@ -250,14 +195,14 @@ computAngelFor1ExpDesign <- function(object, expDegI, var="volume", treatment.on
 #' @return a \code{data.fram} with treatment, control and batch.name
 setGeneric(name = "calculateAngle",
            def = function(object, ExpDesign=NULL, var="volume", treatment.only=TRUE,
-                          plot=TRUE, log.y=TRUE)
+                          plot=TRUE, log.y=FALSE)
                           {standardGeneric("calculateAngle")} )
 
 #' @export
 setMethod( f=calculateAngle,
            signature=c(object="XevaSet"),
            definition= function(object, ExpDesign=NULL, var="volume", treatment.only=TRUE,
-                                plot=TRUE, log.y=TRUE)
+                                plot=TRUE, log.y=FALSE)
            {
              if(!is.null(ExpDesign))
              { ExpDesign = list(ExpDesign) }
