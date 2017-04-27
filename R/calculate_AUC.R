d708a9a2dfc4b6a416b9354e839e53edfd4ff20a
spocks <arvindmer@gmail.com>
2017-04-27 14:12:34 -0400

    chnaged

--- R/calculate_AUC.R
+++ R/calculate_AUC.R
@@ -29,12 +29,12 @@
 ##-------- aac for 1 model ------------------------------
 .getAAC <- function(x, y)
 {
-  x <- .normalize01(x)
+  #x <- .normalize01(x)
   #y <- .normalize01(y)
   #aac <- 1- ( .trapz(x, y) )
+
+  x <- x/x[1]
   aac <- .trapz(x, y)
-  #aacFull <- .trapz(c(x[1], x[length(x)]), c(y[1], y[length(y)]))
-  #aac <- (2*aacFull) - aac
   return(aac)
 }
 
