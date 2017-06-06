0e1d2f670aabcd2103a0e14f02c47ab00df375a8
spocks <arvindmer@gmail.com>
2017-06-06 11:27:52 -0400

    added subsetXeva function

---
+++ R/subset_Xeva.R
@@ -0,0 +1,115 @@
+.resetBatchDesign <- function(batch, vid)
+{
+  rtx <- list()
+  for(i in names(batch))
+  {
+    rtB <- list()
+    rtB$batch.name <- batch[[i]]$batch.name
+
+    if(!is.null(batch[[i]]$treatment))
+    {
+      rtB$treatment <- batch[[i]]$treatment[batch[[i]]$treatment %in% vid]
+      if(length(rtB$treatment)==0){ rtB$treatment<- NULL}
+    }
+
+    if(!is.null(batch[[i]]$control))
+    {
+      rtB$control <- batch[[i]]$control[batch[[i]]$control %in% vid]
+      if(length(rtB$control)==0){ rtB$control<- NULL}
+    }
+
+    rtx[[i]] <- rtB
+    if(is.null(rtx[[i]]$treatment) & is.null(rtx[[i]]$control))
+    {rtx[[i]] <- NULL}
+  }
+  return(rtx)
+}
+
+
+
+####
+#load("~/CXP/XG/Gao2015_Xeva_DataProcess/pdxe.rda")
+#object <- pdxe
+#id <- c("X-1683")
+#id.name <- "patient.id"
+
+#' Subset Xeva object
+#'
+#' @examples
+#' data(pdxe)
+#' df <- subsetXeva(pdxe, ids = c("X-1008", "X-1027"), id.name="patient.id", keep.batch=TRUE)
+#' @param object the \code{XevaSet}
+#' @param ids ids to be selected for
+#' @param id.name names of the id
+#' @param keep.batch Default is \code{TRUE}. If FALSE will remove all the other model.ids from the experiemt design that do not belong to selection
+#' @return New Xeva object
+#' @export
+subsetXeva <- function(object, ids, id.name, keep.batch=TRUE)
+{
+  md <- modelInfo(object)
+  if(is.element(id.name, colnames(md))==FALSE)
+  {
+    msg <- sprintf("'id.name = %s' not present in modelInfo\nValid 'id.name' are\n%s\n",
+                   id.name, paste(colnames(md), collapse = "\n"))
+    stop(msg)
+  }
+
+  mdn <- md[md[, id.name] %in% c(ids), ]
+  if(nrow(mdn)==0)
+  {
+    warning("No model for input ids present, returning NULL")
+    return(NULL)
+  }
+
+  expDesign <- slot(object, "expDesign")
+  expDeNew <- list()
+  for(i in 1:length(expDesign))
+  {
+    bn <- expDesign[[i]]$batch.name
+    tr <- expDesign[[i]]$treatment
+    ct <- expDesign[[i]]$control
+    if(!is.null(tr) & any(tr %in% mdn$model.id))
+    {
+      expDeNew[[bn]] <- expDesign[[i]]
+      next()
+    }
+
+    if(!is.null(ct) & any(ct %in% mdn$model.id))
+    {
+      expDeNew[[bn]] <- expDesign[[i]]
+      next()
+    }
+  }
+
+  if(keep.batch==FALSE)
+  { expDeNew <- .resetBatchDesign(expDeNew, mdn$model.id) }
+
+  nwModId <- unique( unlist(lapply(expDeNew,
+                                   function(x){c(x[["treatment"]],x[["control"]])})))
+  newModId <- unique(c(mdn$model.id, nwModId))
+  mdn <- md[newModId, ]
+
+  slot(object, "expDesign") <- expDeNew
+  slot(object, "experiment") <- slot(object, "experiment")[mdn$model.id]
+  slot(object, "model") <- slot(object, "model")[mdn$model.id, ]
+  slot(object, "drug")  <- slot(object, "drug")[slot(object, "drug")$drug.id %in% mdn$drug,]
+  sn <- slot(object, "sensitivity")
+  sn$model <- sn$model[sn$model$model.id %in% mdn$model.id, ]
+  sn$batch <- sn$batch[sn$batch$batch.name %in% names(expDeNew),]
+  slot(object, "sensitivity") <- sn
+
+  m2b <- slot(object, "modToBiobaseMap")
+  slot(object, "modToBiobaseMap") <- m2b[m2b$model.id %in% mdn$model.id, ]
+  m2b <- slot(object, "modToBiobaseMap")
+  for(mold in names(slot(object, "molecularProfiles")) )
+  {
+    ids2take <- unique(m2b[m2b$mDataType== mold, "biobase.id"])
+    mol <- slot(object, "molecularProfiles")[[mold]]
+    slot(object, "molecularProfiles")[[mold]] <- mol[, ids2take]
+  }
+  return(object)
+}
+
+
+
+
