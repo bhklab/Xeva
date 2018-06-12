16c552882bd321f661d6185dd80f4b4d31875af1
spocks <arvindmer@gmail.com>
2018-06-12 11:11:34 -0400

    added data descrioption

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' Example dataset with 1x1x1 experiment design
+#'
+#' This is PDXE dataset without microarray data.
+#'
+#' @format A Xeva object with 1x1x1 experiment design and moleculer data
+"pdxe"
