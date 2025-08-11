c5ba2609ef34250ba925e364a68ca16d377bd65e
Michael Tran <mtran256@uwo.ca>
2025-08-11 11:45:07 -0400

    Revert "fix: update DESCRIPTION (R >= 4.5.0) and add URL"
    This reverts commit 38f53a5765e763ababc7519adc1223bb9ff7ed84.

--- R/XevaSet_Class.R
+++ R/XevaSet_Class.R
@@ -78,7 +78,7 @@
     is(modelSensitivity, "data.frame") == FALSE |
       is(batchSensitivity, "data.frame") == FALSE
   ) {
-    stop("modelSensitivity and batchSensitivity must be data.frame")
+    stop("slot class error")
   }
 
   rtx <- list(model = modelSensitivity, batch = batchSensitivity)
@@ -100,7 +100,7 @@
 
   for (I in expSlot) {
     if (is.element(slot(I, "model.id"), model$model.id) == FALSE) {
-      msg <- sprintf(
+      msg = sprintf(
         "No informaton present in Model datafram about model.id =%s",
         I$model.id
       )
--- R/create_expDesign_slot.R
+++ R/create_expDesign_slot.R
@@ -1,25 +1,25 @@
 .checkExperimentDesign <- function(expDesign) {
-  modNoControl <- c()
-  modNoTreatme <- c()
+  modNoControl = c()
+  modNoTreatme = c()
   for (I in expDesign) {
     if (length(I$control) == 0 & length(I$treatment) == 0) {
-      stop("Treatment and control are missing in expDesign")
+      stop("Error Treatmetn and Control are missing in expDesign!")
     }
 
     if (length(I$control) == 0) {
-      modNoControl <- c(modNoControl, I$treatment)
+      modNoControl = c(modNoControl, I$treatment)
     }
     if (length(I$treatment) == 0) {
-      modNoTreatme <- c(modNoTreatme, I$control)
+      modNoTreatme = c(modNoTreatme, I$control)
     }
   }
 
-  if (length(modNoControl) > 0) {
-    txt <- sprintf(
+  if (!is.null(modNoControl)) {
+    txt = sprintf(
       "These models have no Controls\n%s\n",
       paste(unique(modNoControl), collapse = "\n")
     )
-    message(txt)
+    cat(txt)
   }
 
   ##------- setting name -----------------
